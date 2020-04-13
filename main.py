95956fe8fc89ad49587cfea0c3fe583b19fd6541
kulvirvirk <kvirkdev@gmail.com>
2020-04-13 01:32:12 +0000

    assigned the values of x, y, and z

--- main.py
+++ main.py
@@ -4,7 +4,10 @@
 # 3. Determine the Pythagorean distance between x, y, and z.
 # 4. Include comments to clarify each line of code.
 
-
+# 1. 
 """This program  will determine the Pythagorean distance between three points."""
+
+#2. 
+x, y, z = 2, 3, 4
 
 
