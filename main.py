642e2d68055bb090645d6d9a863825a333fa84e8
kulvirvirk <kvirkdev@gmail.com>
2020-04-13 01:30:36 +0000

    Requirements

---
+++ main.py
@@ -0,0 +1,8 @@
+# Instructions
+# 1. Write a docstring that describes what is going to happen.
+# 2. Set x, y, and z as equal to 2, 3, and 4.
+# 3. Determine the Pythagorean distance between x, y, and z.
+# 4. Include comments to clarify each line of code.
+
+
+
