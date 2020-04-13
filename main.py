94d99b112ca7560c2cff67f70c929fa85ceabf9a
kulvirvirk <kvirkdev@gmail.com>
2020-04-13 01:44:02 +0000

    calculated the Pythagorean distance

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # Instructions
 # 1. Write a docstring that describes what is going to happen.
 # 2. Set x, y, and z as equal to 2, 3, and 4.
-# 3. Determine the Pythagorean distance between x, y, and z.
+# 3. Determine the Pythagorean distance between x, y, and z. c = √(x^2 + y^2 + z^2)
 # 4. Include comments to clarify each line of code.
+
+import math
 
 # 1. 
 """This program  will determine the Pythagorean distance between three points."""
 
 #2. 
 x, y, z = 2, 3, 4
+
+#3. 
+c_squared = x**2 + y**2 + z**2 
+c = math.sqrt(c_squared)
 
 
